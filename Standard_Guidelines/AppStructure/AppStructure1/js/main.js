bf0a1d0bbaa5a02b4a9e46eecd722b61ab8ea097
Jignesh-UI <hellojiger@gmail.com>
2017-03-10 11:16:42 +0530

    Added pdf,ppt

---
+++ Standard_Guidelines/AppStructure/AppStructure1/js/main.js
@@ -0,0 +1,11 @@
+/* Global variable definitions */
+var ;
+
+
+/* function section */
+function() {
+	
+}
+
+/* Page level listeners should be defined at the end */
+// objName.addEventListener("click", listenerMethodName);
---
+++ Standard_Guidelines/SASS_SCSS/AppStructure/js/app.js
@@ -0,0 +1,117 @@
+var app = angular.module('mis-controls', ['ui.bootstrap'])
+.controller('AlertDemoCtrl', function ($scope) {
+  $scope.alerts = [
+    { type: 'danger', msg: 'Oh snap! Change a few things up and try submitting again.' },
+    { type: 'success', msg: 'Well done! You successfully read this important alert message.' }
+  ];
+
+  $scope.addAlert = function() {
+    $scope.alerts.push({msg: 'Another alert!'});
+  };
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
+})
+.controller('TabsDemoCtrl', function ($scope, $window, $http) {
+	$scope.tabs = [
+		{ title:'HTML',
+			//url: 'js/templates/one.tpl.html'},
+			url: 'one.tpl.html'},
+		{ title:'Javascript',
+			url: 'two.tpl.html'}
+	];
+	  
+	$scope.currentTab = 'one.tpl.html';
+	 
+	$scope.onClickTab = function (tab) {
+        $scope.currentTab = tab.url;
+		
+		$http.get(tab.url).then(function(result){
+            //$scope.content = result.data;
+			alert(result.data);
+        });
+    }
+	
+	$scope.isActiveTab = function(tabUrl) {
+        return tabUrl == $scope.currentTab;
+    }
+  
+})
+.controller('DatepickerDemoCtrl', function ($scope) {
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+})
+.controller('DatepickerDemoCtrl', function ($scope) {
+  $scope.today = function() {
+    $scope.dt = new Date();
+  };
+  $scope.today();
+
+  $scope.clear = function () {
+    $scope.dt = null;
+  };
+
+  // Disable weekend selection
+  $scope.disabled = function(date, mode) {
+    return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+  };
+
+  $scope.toggleMin = function() {
+    $scope.minDate = $scope.minDate ? null : new Date();
+  };
+  $scope.toggleMin();
+
+  $scope.open = function($event) {
+    $event.preventDefault();
+    $event.stopPropagation();
+
+    $scope.opened = true;
+  };
+
+  $scope.dateOptions = {
+    formatYear: 'yy',
+    startingDay: 1
+  };
+
+  $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+  $scope.format = $scope.formats[0];
+})
+.controller('sortCtrl', ['$scope', '$filter', function (scope, filter) {
+    scope.rowCollection = [
+        {firstName: 'Laurent', lastName: 'Renard', birthDate: new Date('1987-05-21'), balance: 102, email: 'whatever@gmail.com'},
+        {firstName: 'Blandine', lastName: 'Faivre', birthDate: new Date('1987-04-25'), balance: -2323.22, email: 'oufblandou@gmail.com'},
+        {firstName: 'Francoise', lastName: 'Frere', birthDate: new Date('1955-08-27'), balance: 42343, email: 'raymondef@gmail.com'}
+    ];    
+}]);
